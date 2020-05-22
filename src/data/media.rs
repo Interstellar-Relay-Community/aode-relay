4df92f51282cd4464028dfdbfb8c84030c2ca5f9
asonix <asonix@asonix.dog>
2020-05-21 21:42:56 -0500

    Use a better mutex

--- src/data/media.rs
+++ src/data/media.rs
@@ -1,10 +1,11 @@
 use crate::{db::Db, error::MyError};
 use activitystreams_new::primitives::XsdAnyUri;
+use async_mutex::Mutex;
 use bytes::Bytes;
 use futures::join;
 use lru::LruCache;
 use std::{collections::HashMap, sync::Arc, time::Duration};
-use tokio::sync::{Mutex, RwLock};
+use tokio::sync::RwLock;
 use ttl_cache::TtlCache;
 use uuid::Uuid;
 
