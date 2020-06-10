aaa54d26ccd092310258553d8b95eb2d7969f250
asonix <asonix@asonix.dog>
2020-06-10 17:45:59 -0500

    Version v0.1.0-r103

---
+++ docker/prod/deploy.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+TAG=$1
+MIGRATIONS=$2
+
+function require() {
+    if [ "$1" = "" ]; then
+        echo "input '$2' required"
+        print_help
+        exit 1
+    fi
+}
+
+function print_help() {
+    echo "build.sh"
+    echo ""
+    echo "Usage:"
+    echo "      build.sh [tag] [migrations]"
+    echo ""
+    echo "Args:"
+    echo "      tag: The git tag to create and publish"
+    echo "      migrations: (optional) Whether to build the migrations container as well"
+}
+
+function build_image() {
+    repo=$1
+    tag=$2
+    arch=$3
+
+    docker build \
+        --pull \
+        --build-arg TAG="${tag}" \
+        -f "Dockerfile.${arch}" \
+        -t "${repo}:${tag}-${arch}" \
+        -t "${repo}:latest-${arch}" \
+        ./artifacts
+
+    docker push "${repo}:${tag}-arm64v8"
+    docker push "${repo}:latest-arm64v8"
+}
+
+require "$TAG" "tag"
+
+if ! docker run --rm -it arm64v8/ubuntu:19.10 /bin/bash -c 'echo "docker is configured correctly"'; then
+    echo "docker is not configured to run on qemu-emulated architectures, fixing will require sudo"
+    sudo docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+fi
+
+set -xe
+
+git checkout master
+git commit -m "Version $TAG"
+git tag $TAG
+
+git push origin $TAG
+git push
+
+build_image "asonix/relay" "$TAG" "arm64v8"
+build_image "asonix/relay" "$TAG" "arm32v7"
+build_image "asonix/relay" "$TAG" "amd64"
+
+./manifest.sh "asonix/relay" "$TAG"
+./manifest.sh "asonix/relay" "latest"
+
+if [ "${MIGRATIONS}" = "migrations" ]; then
+    build_image "asonix/relay-migrations" "$TAG" arm64v8
+    build_image "asonix/relay-migrations" "$TAG" arm32v7
+    build_image "asonix/relay-migrations" "$TAG" amd64
+
+    ./manifest.sh "asonix/relay-migrations" "$TAG"
+    ./manifest.sh "asonix/relay-migrations" "latest"
+fi
---
+++ docker/prod/manifest.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+function require() {
+    if [ "$1" = "" ]; then
+        echo "input '$2' required"
+        print_help
+        exit 1
+    fi
+}
+function print_help() {
+    echo "deploy.sh"
+    echo ""
+    echo "Usage:"
+    echo "	manifest.sh [tag]"
+    echo ""
+    echo "Args:"
+    echo "	repo: The docker repository to push the manifest to"
+    echo "	tag: The git tag to be applied to the image manifest"
+}
+
+repo=$2
+tag=$2
+
+require "$repo" "repo"
+require "$tag" "tag"
+
+set -xe
+
+docker manifest create $repo:$tag \
+    -a $repo:arm64v8-$tag \
+    -a $repo:arm32v7-$tag \
+    -a $repo:amd64-$tag
+
+docker manifest annotate $repo:$tag \
+    $repo:arm64v8-$tag --os linux --arch arm64 --variant v8
+
+docker manifest annotate $repo:$tag \
+    $repo:arm32v7-$tag --os linux --arch arm --variant v7
+
+docker manifest annotate $repo:$tag \
+    $repo:amd64-$tag --os linux --arch amd64
+
+docker manifest push $repo:$tag --purge
