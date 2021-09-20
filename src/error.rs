90965d6ee9a651e69159df9431775d3a3ae3c12c
Aode (lion) <asonix@asonix.dog>
2021-09-20 13:13:21 -0500

    Better format errors

--- src/error.rs
+++ src/error.rs
@@ -17,7 +17,7 @@ pub(crate) struct Error {
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.kind)?;
+        write!(f, "{}\n", self.kind)?;
         std::fmt::Display::fmt(&self.context, f)
     }
 }
