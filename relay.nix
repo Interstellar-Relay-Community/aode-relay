a9a47e8ee2ec3efd4d0bacd791a9623069fb079c
asonix <asonix@asonix.dog>
2023-04-27 19:54:15 -0500

    Update flake

--- relay.nix
+++ relay.nix
@@ -10,7 +10,7 @@ rustPlatform.buildRustPackage {
   pname = "relay";
   version = "0.4.0-beta.1";
   src = ./.;
-  cargoSha256 = "Kbicf3alz/QZJppBH4+dRhIf4extIONwBYnGF3TtqJ4=";
+  cargoSha256 = "hzNPI6ODQ208eGQ4xY3dZ+c5O1A3ozUF1emglv3ni/o=";
 
   PROTOC = "${protobuf}/bin/protoc";
   PROTOC_INCLUDE = "${protobuf}/include";
